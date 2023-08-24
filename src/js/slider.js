7cde12bc6db39eaf40d7d443acb077460b712594
Татьяна Хонякина <hanakinat@gmail.com>
2023-08-24 22:19:27 +0300

    Изменены пути до файла

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,45 +1,45 @@
-const slider = document.querySelector('.slider_container')
-const slides = document.querySelectorAll('.slide');
-const back = document.querySelector('.back');
-const forward = document.querySelector('.forward');
+document.addEventListener('DOMContentLoaded', () => {
+    const slider = document.querySelector('.slider_container')
+    const slides = document.querySelectorAll('.slide');
+    const back = document.querySelector('.back');
+    const forward = document.querySelector('.forward');
 
+    const maxSlides = slides.length;
+    let currentSlide = 0;
 
+    back.addEventListener('click', swipeBack);
+    forward.addEventListener('click', swipeForward);
 
-const maxSlides = slides.length;
-let currentSlide = 0;
+    function checkButtonsState() {
+        back.classList.remove('inactive_slider_button');
+        forward.classList.remove('inactive_slider_button');
 
-back.addEventListener('click', swipeBack);
-forward.addEventListener('click', swipeForward);
+        if (currentSlide === maxSlides - 1) {
+            forward.classList.add('inactive_slider_button');
+        }
 
-function checkButtonsState() {
-    back.classList.remove('inactive_slider_button');
-    forward.classList.remove('inactive_slider_button');
-
-    if (currentSlide === maxSlides - 1) {
-        forward.classList.add('inactive_slider_button');
+        if (currentSlide === 0) {
+            back.classList.add('inactive_slider_button');
+        }
     }
 
-    if (currentSlide === 0) {
-        back.classList.add('inactive_slider_button');
-    }
-}
+    function swipeBack() {
+        if (currentSlide === 0) {
+            return;
+        }
 
-function swipeBack() {
-    if (currentSlide === 0) {
-        return;
-    }
-
-    currentSlide--;
-    slider.scroll((slides[0].clientWidth + 15) * currentSlide, 0);
-    checkButtonsState();
-}
-
-function swipeForward() {
-    if (currentSlide === maxSlides - 1) {
-        return;
+        currentSlide--;
+        slider.scroll((slides[0].clientWidth + 15) * currentSlide, 0);
+        checkButtonsState();
     }
 
-    currentSlide++;
-    slider.scroll((slides[0].clientWidth + 15) * currentSlide, 0);
-    checkButtonsState();
-}
+    function swipeForward() {
+        if (currentSlide === maxSlides - 1) {
+            return;
+        }
+
+        currentSlide++;
+        slider.scroll((slides[0].clientWidth + 15) * currentSlide, 0);
+        checkButtonsState();
+    }
+})
